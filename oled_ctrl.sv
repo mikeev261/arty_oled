8606bef4612b615df9b4407cca7658c0e7589003
mikeev261 <mikeev@gmail.com>
2017-03-03 22:55:16 -0800

    Adding initial CI of oled_ctrl and spi_m modules... to be filled-in later.

---
+++ oled_ctrl.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/03/2017 10:44:57 PM
+// Design Name: 
+// Module Name: oled_ctrl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments: https://reference.digilentinc.com/_media/reference/pmod/pmodoledrgb/pmodoledrgb_rm.pdf
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module oled_ctrl(
+
+    );
+endmodule
---
+++ spi_m.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/03/2017 10:44:57 PM
+// Design Name: 
+// Module Name: spi_m
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module spi_m(
+
+    );
+endmodule
